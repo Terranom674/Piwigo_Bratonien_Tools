f2ef62f58023a3eab57526d70d2cc8ace60f111b
Terranom674 <thomas_dannenberg@web.de>
2026-08-15 11:33:43 +0200

    Add Piwigo plugin maintenance class

---
+++ maintain.class.php
@@ -0,0 +1,28 @@
+<?php
+if (!defined('PHPWG_ROOT_PATH'))
+{
+  die('Hacking attempt!');
+}
+
+class BratonienTools_maintain extends PluginMaintain
+{
+  public function install($plugin_version, &$errors = array())
+  {
+  }
+
+  public function activate($plugin_version, &$errors = array())
+  {
+  }
+
+  public function deactivate()
+  {
+  }
+
+  public function update($old_version, $new_version, &$errors = array())
+  {
+  }
+
+  public function uninstall()
+  {
+  }
+}
