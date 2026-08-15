c988a6efdc4fe1613b59ba53032c18ee66406983
Terranom674 <thomas_dannenberg@web.de>
2026-08-15 20:09:22 +0200

    Remove obsolete watermark precache call from plugin maintenance

--- maintain.class.php
+++ maintain.class.php
@@ -6,7 +6,6 @@ if (!defined('PHPWG_ROOT_PATH'))
 
 require_once(dirname(__FILE__) . '/include/database.class.php');
 require_once(dirname(__FILE__) . '/tools/watermark_profiles.inc.php');
-require_once(dirname(__FILE__) . '/tools/image_cache.inc.php');
 
 class bratonien_tools_maintain extends PluginMaintain
 {
@@ -14,7 +13,6 @@ class bratonien_tools_maintain extends PluginMaintain
   {
     bratonien_tools_create_tables();
     bratonien_tools_create_default_watermark_profiles();
-    bratonien_tools_request_watermark_precache();
   }
 
   public function install($plugin_version, &$errors = array())
