9e940b02288740c22d930bf5ffc2db5d9ba20a22
Terranom674 <thomas_dannenberg@web.de>
2026-08-15 13:30:22 +0200

    Initialize watermark profile defaults during plugin setup

--- maintain.class.php
+++ maintain.class.php
@@ -5,17 +5,20 @@ if (!defined('PHPWG_ROOT_PATH'))
 }
 
 require_once(dirname(__FILE__) . '/include/database.class.php');
+require_once(dirname(__FILE__) . '/tools/watermark_profiles.inc.php');
 
 class bratonien_tools_maintain extends PluginMaintain
 {
   public function install($plugin_version, &$errors = array())
   {
     bratonien_tools_create_tables();
+    bratonien_tools_create_default_watermark_profiles();
   }
 
   public function activate($plugin_version, &$errors = array())
   {
     bratonien_tools_create_tables();
+    bratonien_tools_create_default_watermark_profiles();
   }
 
   public function deactivate()
@@ -25,6 +28,7 @@ class bratonien_tools_maintain extends PluginMaintain
   public function update($old_version, $new_version, &$errors = array())
   {
     bratonien_tools_create_tables();
+    bratonien_tools_create_default_watermark_profiles();
   }
 
   public function uninstall()
