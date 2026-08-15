2a863f08961566a3d5219e629f727189eda0e7fd
Terranom674 <thomas_dannenberg@web.de>
2026-08-15 13:21:20 +0200

    Initialize watermark tables on plugin install

--- maintain.class.php
+++ maintain.class.php
@@ -4,14 +4,18 @@ if (!defined('PHPWG_ROOT_PATH'))
   die('Hacking attempt!');
 }
 
+require_once(dirname(__FILE__) . '/include/database.class.php');
+
 class bratonien_tools_maintain extends PluginMaintain
 {
   public function install($plugin_version, &$errors = array())
   {
+    bratonien_tools_create_tables();
   }
 
   public function activate($plugin_version, &$errors = array())
   {
+    bratonien_tools_create_tables();
   }
 
   public function deactivate()
@@ -20,9 +24,11 @@ class bratonien_tools_maintain extends PluginMaintain
 
   public function update($old_version, $new_version, &$errors = array())
   {
+    bratonien_tools_create_tables();
   }
 
   public function uninstall()
   {
+    bratonien_tools_drop_tables();
   }
 }
