ddb1a9d4db7812cf146ec85cf9be94bc7bfe41c1
Terranom674 <thomas_dannenberg@web.de>
2026-08-17 00:04:58 +0200

    Bump version to 0.13.9

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Bratonien Tools
-Version: 0.13.8
+Version: 0.13.9
 Description: Erweiterbare Administrationswerkzeuge fuer die Bratonien-Piwigo-Installation.
 Plugin URI: https://github.com/Terranom674/Piwigo_Bratonien_Tools
 Author: Bratonien
