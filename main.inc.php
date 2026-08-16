7b4f2d63480fc992ef643f0bf270bacf5653ba30
Terranom674 <thomas_dannenberg@web.de>
2026-08-16 23:12:08 +0200

    Keep editor access when private albums are created

--- main.inc.php
+++ main.inc.php
@@ -25,6 +25,7 @@ add_event_handler('get_admin_plugin_menu_links', 'bratonien_tools_admin_menu');
 add_event_handler('get_derivative_url', 'bratonien_tools_filter_derivative_url', EVENT_HANDLER_PRIORITY_NEUTRAL, 4);
 add_event_handler('loc_end_element_set_global', 'bratonien_tools_batch_titles_register_action');
 add_event_handler('element_set_global_action', 'bratonien_tools_batch_titles_apply', EVENT_HANDLER_PRIORITY_NEUTRAL, 2);
+add_event_handler('init', 'bratonien_tools_preserve_private_album_access', EVENT_HANDLER_PRIORITY_NEUTRAL - 10);
 add_event_handler('init', 'bratonien_tools_album_shares_init');
 add_event_handler('delete_categories', 'bratonien_tools_album_shares_on_delete_categories');
 
