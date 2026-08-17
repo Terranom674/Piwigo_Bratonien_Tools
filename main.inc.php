c932aacd79c44b7d7a385e481bf5f77da3e6c5bb
Terranom674 <thomas_dannenberg@web.de>
2026-08-17 12:42:18 +0200

    Renumber current release to 0.10.4

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Bratonien Tools
-Version: 0.14.3
+Version: 0.10.4
 Description: Erweiterbare Administrationswerkzeuge fuer die Bratonien-Piwigo-Installation.
 Plugin URI: https://github.com/Terranom674/Piwigo_Bratonien_Tools
 Author: Bratonien
