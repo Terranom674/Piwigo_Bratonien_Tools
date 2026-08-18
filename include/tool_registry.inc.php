d61e239a244f896af432724fca9c731c87eee6e6
Terranom674 <thomas_dannenberg@web.de>
2026-08-18 07:52:16 +0200

    Register NC wizard and connection edit handlers

--- include/tool_registry.inc.php
+++ include/tool_registry.inc.php
@@ -21,109 +21,49 @@ require_once(BRATONIEN_TOOLS_PATH . 'include/nc_connector_takeover.inc.php');
 require_once(BRATONIEN_TOOLS_PATH . 'include/nc_connector_auth.inc.php');
 require_once(BRATONIEN_TOOLS_PATH . 'include/nc_connector_create_api.inc.php');
 require_once(BRATONIEN_TOOLS_PATH . 'include/nc_connector_piwigo_api.inc.php');
+require_once(BRATONIEN_TOOLS_PATH . 'include/nc_connector_wizard.inc.php');
 
 function bratonien_tools_get_tools()
 {
   return array(
-    'image_cache_clear' => array(
-      'handler' => 'bratonien_tools_clear_image_cache',
-    ),
-    'image_cache_build' => array(
-      'handler' => 'bratonien_tools_start_main_cache_build',
-    ),
-    'image_cache_cancel' => array(
-      'handler' => 'bratonien_tools_cancel_main_cache_build',
-    ),
-    'image_cache_worker_settings' => array(
-      'handler' => 'bratonien_tools_save_cache_worker_settings',
-    ),
-    'watermark_save' => array(
-      'handler' => 'bratonien_tools_save_watermark',
-    ),
-    'watermark_file_delete' => array(
-      'handler' => 'bratonien_tools_delete_watermark_file',
-    ),
-    'watermark_engine' => array(
-      'handler' => 'bratonien_tools_handle_watermark_engine',
-    ),
-    'watermark_profile_save' => array(
-      'handler' => 'bratonien_tools_save_watermark_profile',
-    ),
-    'watermark_profile_delete' => array(
-      'handler' => 'bratonien_tools_delete_watermark_profile',
-    ),
-    'watermark_profile_duplicate' => array(
-      'handler' => 'bratonien_tools_duplicate_watermark_profile',
-    ),
-    'watermark_defaults' => array(
-      'handler' => 'bratonien_tools_save_watermark_defaults',
-    ),
-    'watermark_rule' => array(
-      'handler' => 'bratonien_tools_save_album_rule',
-    ),
-    'public_selection_settings' => array(
-      'handler' => 'bratonien_tools_save_public_selection_settings',
-    ),
-    'asset_upload' => array(
-      'handler' => 'bratonien_tools_upload_asset',
-    ),
-    'asset_delete' => array(
-      'handler' => 'bratonien_tools_delete_asset',
-    ),
-    'asset_upload_limits' => array(
-      'handler' => 'bratonien_tools_save_upload_limits',
-    ),
-    'album_lock_toggle' => array(
-      'handler' => 'bratonien_tools_toggle_album_lock',
-    ),
-    'album_share_create' => array(
-      'handler' => 'bratonien_tools_create_album_share',
-    ),
-    'album_share_regenerate_link' => array(
-      'handler' => 'bratonien_tools_regenerate_album_share_link',
-    ),
-    'album_share_revoke' => array(
-      'handler' => 'bratonien_tools_revoke_album_share',
-    ),
-    'nc_connector_create_local' => array(
-      'handler' => 'bratonien_tools_nc_connector_create_local_api_first',
-    ),
-    'nc_connector_delete' => array(
-      'handler' => 'bratonien_tools_nc_connector_delete',
-    ),
-    'nc_connector_import_legacy' => array(
-      'handler' => 'bratonien_tools_nc_connector_import_legacy',
-    ),
-    'nc_connector_verify' => array(
-      'handler' => 'bratonien_tools_nc_connector_verify_managed',
-    ),
-    'nc_connector_prepare_takeover' => array(
-      'handler' => 'bratonien_tools_nc_connector_prepare_takeover',
-    ),
-    'nc_connector_cancel_takeover' => array(
-      'handler' => 'bratonien_tools_nc_connector_cancel_takeover',
-    ),
-    'nc_connector_piwigo_api_test' => array(
-      'handler' => 'bratonien_tools_nc_connector_piwigo_api_test',
-    ),
-    'nc_connector_piwigo_api_delete' => array(
-      'handler' => 'bratonien_tools_nc_connector_api_delete',
-    ),
-    'nc_connector_fallback_save' => array(
-      'handler' => 'bratonien_tools_nc_connector_fallback_save',
-    ),
-    'nc_connector_fallback_delete' => array(
-      'handler' => 'bratonien_tools_nc_connector_fallback_delete',
-    ),
-    'nc_connector_fallback_once' => array(
-      'handler' => 'bratonien_tools_nc_connector_fallback_once',
-    ),
-    'self_update_check' => array(
-      'handler' => 'bratonien_tools_self_update_check',
-    ),
-    'self_update_run' => array(
-      'handler' => 'bratonien_tools_self_update_run',
-    ),
+    'image_cache_clear' => array('handler' => 'bratonien_tools_clear_image_cache'),
+    'image_cache_build' => array('handler' => 'bratonien_tools_start_main_cache_build'),
+    'image_cache_cancel' => array('handler' => 'bratonien_tools_cancel_main_cache_build'),
+    'image_cache_worker_settings' => array('handler' => 'bratonien_tools_save_cache_worker_settings'),
+    'watermark_save' => array('handler' => 'bratonien_tools_save_watermark'),
+    'watermark_file_delete' => array('handler' => 'bratonien_tools_delete_watermark_file'),
+    'watermark_engine' => array('handler' => 'bratonien_tools_handle_watermark_engine'),
+    'watermark_profile_save' => array('handler' => 'bratonien_tools_save_watermark_profile'),
+    'watermark_profile_delete' => array('handler' => 'bratonien_tools_delete_watermark_profile'),
+    'watermark_profile_duplicate' => array('handler' => 'bratonien_tools_duplicate_watermark_profile'),
+    'watermark_defaults' => array('handler' => 'bratonien_tools_save_watermark_defaults'),
+    'watermark_rule' => array('handler' => 'bratonien_tools_save_album_rule'),
+    'public_selection_settings' => array('handler' => 'bratonien_tools_save_public_selection_settings'),
+    'asset_upload' => array('handler' => 'bratonien_tools_upload_asset'),
+    'asset_delete' => array('handler' => 'bratonien_tools_delete_asset'),
+    'asset_upload_limits' => array('handler' => 'bratonien_tools_save_upload_limits'),
+    'album_lock_toggle' => array('handler' => 'bratonien_tools_toggle_album_lock'),
+    'album_share_create' => array('handler' => 'bratonien_tools_create_album_share'),
+    'album_share_regenerate_link' => array('handler' => 'bratonien_tools_regenerate_album_share_link'),
+    'album_share_revoke' => array('handler' => 'bratonien_tools_revoke_album_share'),
+    'nc_connector_create_local' => array('handler' => 'bratonien_tools_nc_connector_create_local_api_first'),
+    'nc_connector_delete' => array('handler' => 'bratonien_tools_nc_connector_delete'),
+    'nc_connector_update_name' => array('handler' => 'bratonien_tools_nc_connector_update_name'),
+    'nc_connector_update_technical' => array('handler' => 'bratonien_tools_nc_connector_update_technical'),
+    'nc_connector_wizard_scan' => array('handler' => 'bratonien_tools_nc_wizard_scan'),
+    'nc_connector_wizard_select_user' => array('handler' => 'bratonien_tools_nc_wizard_select_user'),
+    'nc_connector_wizard_reset' => array('handler' => 'bratonien_tools_nc_wizard_reset'),
+    'nc_connector_import_legacy' => array('handler' => 'bratonien_tools_nc_connector_import_legacy'),
+    'nc_connector_verify' => array('handler' => 'bratonien_tools_nc_connector_verify_managed'),
+    'nc_connector_prepare_takeover' => array('handler' => 'bratonien_tools_nc_connector_prepare_takeover'),
+    'nc_connector_cancel_takeover' => array('handler' => 'bratonien_tools_nc_connector_cancel_takeover'),
+    'nc_connector_piwigo_api_test' => array('handler' => 'bratonien_tools_nc_connector_piwigo_api_test'),
+    'nc_connector_piwigo_api_delete' => array('handler' => 'bratonien_tools_nc_connector_api_delete'),
+    'nc_connector_fallback_save' => array('handler' => 'bratonien_tools_nc_connector_fallback_save'),
+    'nc_connector_fallback_delete' => array('handler' => 'bratonien_tools_nc_connector_fallback_delete'),
+    'nc_connector_fallback_once' => array('handler' => 'bratonien_tools_nc_connector_fallback_once'),
+    'self_update_check' => array('handler' => 'bratonien_tools_self_update_check'),
+    'self_update_run' => array('handler' => 'bratonien_tools_self_update_run'),
   );
 }
 
