898f9b1bc95fb3a6460e5f5d1f1077301927e9bb
Terranom674 <thomas_dannenberg@web.de>
2026-08-19 13:58:08 +0200

    0.9.6.11 bump version for dialog validation

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Bratonien Tools
-Version: 0.9.6.10
+Version: 0.9.6.11
 Description: Erweiterbare Administrationswerkzeuge fuer die Bratonien-Piwigo-Installation.
 Plugin URI: https://github.com/Terranom674/Piwigo_Bratonien_Tools
 Author: Bratonien
