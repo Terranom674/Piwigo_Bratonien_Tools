a5be16f88e1fcce8fc5fd4178bf2dce31f322c7a
Terranom674 <thomas_dannenberg@web.de>
2026-08-19 20:40:40 +0200

    0.9.7.1: stream WebDAV originals through resolved target IP

--- webdav-image.php
+++ webdav-image.php
@@ -8,6 +8,7 @@ if (!defined('BRATONIEN_TOOLS_PATH'))
   define('BRATONIEN_TOOLS_PATH', PHPWG_ROOT_PATH.'plugins/'.BRATONIEN_TOOLS_ID.'/');
 }
 require_once(BRATONIEN_TOOLS_PATH.'include/webdav_image_runtime.inc.php');
+require_once(BRATONIEN_TOOLS_PATH.'include/nc_transport.inc.php');
 
 function bratonien_tools_webdav_image_abort($status, $message)
 {
@@ -120,7 +121,7 @@ $options = array(
   CURLOPT_USERPWD => $user.':'.$password,
   CURLOPT_RETURNTRANSFER => false,
   CURLOPT_FAILONERROR => false,
-  CURLOPT_USERAGENT => 'Bratonien-Tools-WebDAV-Image/0.9.6.1',
+  CURLOPT_USERAGENT => 'Bratonien-Tools-WebDAV-Image/0.9.7.1',
   CURLOPT_HEADERFUNCTION => function($ch, $line)
   {
     $length = strlen($line);
@@ -147,6 +148,7 @@ $options = array(
     return strlen($data);
   },
 );
+bratonien_tools_nc_transport_apply_curl($options, $url);
 if (!empty($_SERVER['HTTP_RANGE']))
 {
   $range = trim((string)$_SERVER['HTTP_RANGE']);
