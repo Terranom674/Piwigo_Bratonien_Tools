29990aee4f67f9166fc7f5af27674b24cb27e1b3
Terranom674 <thomas_dannenberg@web.de>
2026-08-20 09:00:53 +0200

    0.9.7.24: Version anheben

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Bratonien Tools
-Version: 0.9.7.23
+Version: 0.9.7.24
 Description: Erweiterbare Administrationswerkzeuge fuer die Bratonien-Piwigo-Installation.
 Plugin URI: https://github.com/Terranom674/Piwigo_Bratonien_Tools
 Author: Bratonien
