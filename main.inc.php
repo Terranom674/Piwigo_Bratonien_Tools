81e0a9e185a14c08230b4f8fa0f36809c5b2b9a7
Terranom674 <thomas_dannenberg@web.de>
2026-09-03 18:26:58 +0200

    Bump patch version for WebDAV-only cache build routing

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Bratonien Tools
-Version: 0.9.7.1.15
+Version: 0.9.7.1.16
 Description: Erweiterbare Administrationswerkzeuge fuer die Bratonien-Piwigo-Installation.
 Plugin URI: https://github.com/Terranom674/Piwigo_Bratonien_Tools
 Author: Bratonien
