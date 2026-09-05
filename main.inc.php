acbf316f589bf4b4feb8781af17d3124d4666c1c
Terranom674 <thomas_dannenberg@web.de>
2026-09-05 16:22:16 +0200

    Bump Bratonien Tools version for QR downloads

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Bratonien Tools
-Version: 0.9.7.1.36
+Version: 0.9.7.1.37
 Description: Erweiterbare Administrationswerkzeuge fuer die Bratonien-Piwigo-Installation.
 Plugin URI: https://github.com/Terranom674/Piwigo_Bratonien_Tools
 Author: Bratonien
